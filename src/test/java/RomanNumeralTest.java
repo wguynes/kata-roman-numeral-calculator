79420ef5a463ae04e055c1c9274051f023ff7404
William Guynes <william.a.guynes@accenture.com>
2020-04-12 14:45:51 -0500

    Test simple addition

--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -9,4 +9,18 @@ public class RomanNumeralTest {
         RomanNumeral numeral = new RomanNumeral("I");
         assertEquals("I", numeral.value);
     }
+
+    @Test
+    public void givesIIforSumOfIandI() {
+        RomanNumeral numeral = new RomanNumeral("I");
+        numeral.sum(numeral);
+        assertEquals("II", numeral.value);
+    }
+
+    @Test
+    public void givesIIIforSumOfIandII() {
+        RomanNumeral numeral = new RomanNumeral("I");
+        numeral.sum(new RomanNumeral("II"));
+        assertEquals("III", numeral.value);
+    }
 }
