a2accccd7b9557b092f7c8ef404479f3d4cf82e0
William Guynes <william.a.guynes@accenture.com>
2020-04-12 19:41:29 -0500

    Sums to 9 and 10

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class RomanNumeral {
 
     public String value;
@@ -6,9 +11,41 @@ public class RomanNumeral {
         this.value = value;
     }
 
+    List<Character> sortOrder = Arrays.asList('V','I');
+
     public RomanNumeral sum(RomanNumeral numeral) {
+        this.value = this.value.replaceAll("IX", "VIIII");
+        this.value = this.value.replaceAll("IV", "IIII");
+
+        numeral.value = numeral.value.replaceAll("IX", "VIIII");
+        numeral.value = numeral.value.replaceAll("IV", "IIII");
+
         this.value = new StringBuilder().append(this.value).append(numeral.value).toString();
+
+
+        List<Character> list = new ArrayList<>();
+        for (char ch: this.value.toCharArray()) {
+            list.add(ch);
+        }
+
+        list.sort(new Comparator<Character>() {
+            @Override
+            public int compare(Character left, Character right) {
+                return Integer.compare(sortOrder.indexOf(left), sortOrder.indexOf(right));
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : list) {
+            sb.append(ch);
+        }
+        this.value = sb.toString();
+
+//        this.value = this.value.replaceAll("VIIIII", "X");
         this.value = this.value.replaceAll("IIIII", "V");
+        this.value = this.value.replaceAll("VV", "X");
+        this.value = this.value.replaceAll("VIIII", "IX");
+        this.value = this.value.replaceAll("IIII", "IV");
 
         return this;
     }
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -4,27 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralTest {
 
-    @Test
-    public void takesStringInConstructor() {
-        RomanNumeral numeral = new RomanNumeral("I");
-        assertEquals("I", numeral.value);
-    }
-
-    @Test
-    public void givesIIforSumOfIandI() {
-        assertSum("I", "I", "II");
-    }
-
-    @Test
-    public void givesIIIforSumOfIandII() {
-        assertSum("I", "II", "III");
-    }
-
-    @Test
-    public void givesVforSumOfIIandIII() {
-        assertSum("II", "III", "V");
-    }
-
     void assertSum(String first, String second, String expectedProduct)
     {
         RomanNumeral numeral = new RomanNumeral(first);
@@ -33,13 +12,68 @@ public class RomanNumeralTest {
     }
 
     @Test
-    public void givesVIforSumOfIIIandIII() {
-        assertSum("III", "III", "VI");
+    public void takesStringInConstructor() {
+        RomanNumeral numeral = new RomanNumeral("I");
+        assertEquals("I", numeral.value);
     }
 
     @Test
-    public void givesIVforSumOfIIandII() {
-        assertSum("II", "II", "VI");
+    public void sumsToII() {
+        assertSum("I", "I", "II");
     }
 
+    @Test
+    public void sumsToIII() {
+        assertSum("I", "II", "III");
+        assertSum("II", "I", "III");
+    }
+
+    @Test
+    public void sumsToIV() {
+        assertSum("I", "III", "IV");
+        assertSum("II", "II", "IV");
+        assertSum("III", "I", "IV");
+    }
+
+    @Test
+    public void sumsToV() {
+        assertSum("I", "IV", "V");
+        assertSum("II", "III", "V");
+        assertSum("III", "II", "V");
+        assertSum("IV", "I", "V");
+    }
+
+    @Test
+    public void sumsToVI() {
+        assertSum("I", "V", "VI");
+        assertSum("II", "IV", "VI");
+        assertSum("III", "III", "VI");
+        assertSum("IV", "II", "VI");
+        assertSum("V", "I", "VI");
+    }
+
+    @Test
+    public void sumsToIX() {
+        assertSum("I", "VIII", "IX");
+        assertSum("II", "VII", "IX");
+        assertSum("III", "VI", "IX");
+        assertSum("IV", "V", "IX");
+        assertSum("V", "IV", "IX");
+        assertSum("VI", "III", "IX");
+        assertSum("VII", "II", "IX");
+        assertSum("VIII", "I", "IX");
+    }
+
+    @Test
+    public void sumsToX() {
+        assertSum("I", "IX", "X");
+        assertSum("II", "VIII", "X");
+        assertSum("III", "VII", "X");
+        assertSum("IV", "VI", "X");
+        assertSum("V", "V", "X");
+        assertSum("VI", "IV", "X");
+        assertSum("VII", "III", "X");
+        assertSum("VIII", "II", "X");
+        assertSum("IX", "I", "X");
+    }
 }
