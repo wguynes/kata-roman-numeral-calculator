9c69795cdf66edb83f66ff0cc0a64e67a94c636e
William Guynes <william.a.guynes@accenture.com>
2020-04-13 15:00:13 -0500

    Make Pair better, derive a StringPair

--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -1,12 +1,10 @@
 public class Pair<L,R> {
-    private L l;
-    private R r;
+    private L left;
+    private R right;
     public Pair(L l, R r){
-        this.l = l;
-        this.r = r;
+        this.left = l;
+        this.right = r;
     }
-    public L getL(){ return l; }
-    public R getR(){ return r; }
-    public void setL(L l){ this.l = l; }
-    public void setR(R r){ this.r = r; }
+    public L getLeft(){ return left; }
+    public R getRight(){ return right; }
 }
--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -4,6 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
+class StringPair extends Pair<String,String> {
+    public StringPair(String left, String right) {
+        super(left, right);
+    }
+}
+
 public class RomanNumeral {
 
     public String value;
@@ -13,30 +19,30 @@ public class RomanNumeral {
     }
 
     List<Character> sortOrder = Arrays.asList('M', 'D', 'C', 'L', 'X', 'V', 'I');
-    List<Pair<String, String>> ORDERED_LIST_OF_UNPACKING_PAIRS = Arrays.asList(
-            new Pair<>("CM", "DCCCC"),
-            new Pair<>("CD", "CCCC"),
-            new Pair<>("XL", "XXXX"),
-            new Pair<>("XC", "LXXXX"),
-            new Pair<>("IX", "VIIII"),
-            new Pair<>("IV", "IIII")
+    List<StringPair> ORDERED_LIST_OF_UNPACKING_PAIRS = Arrays.asList(
+            new StringPair("CM", "DCCCC"),
+            new StringPair("CD", "CCCC"),
+            new StringPair("XL", "XXXX"),
+            new StringPair("XC", "LXXXX"),
+            new StringPair("IX", "VIIII"),
+            new StringPair("IV", "IIII")
     );
-    List<Pair<String, String>> ORDERED_LIST_OF_PACKING_PAIRS = Arrays.asList(
+    List<StringPair> ORDERED_LIST_OF_PACKING_PAIRS = Arrays.asList(
             // pack
-            new Pair<>("IIIII", "V"),
-            new Pair<>("VV", "X"),
-            new Pair<>("XXXXX", "L"),
-            new Pair<>("LL", "C"),
-            new Pair<>("CCCCC", "D"),
-            new Pair<>("DD","M"),
+            new StringPair("IIIII", "V"),
+            new StringPair("VV", "X"),
+            new StringPair("XXXXX", "L"),
+            new StringPair("LL", "C"),
+            new StringPair("CCCCC", "D"),
+            new StringPair("DD","M"),
             // reintroduce 9s
-            new Pair<>("DCCCC", "CM"),
-            new Pair<>("LXXXX", "XC"),
-            new Pair<>("VIIII", "IX"),
+            new StringPair("DCCCC", "CM"),
+            new StringPair("LXXXX", "XC"),
+            new StringPair("VIIII", "IX"),
             // reintroduce 4s
-            new Pair<>("CCCC", "CD"),
-            new Pair<>("XXXX", "XL"),
-            new Pair<>("IIII", "IV")
+            new StringPair("CCCC", "CD"),
+            new StringPair("XXXX", "XL"),
+            new StringPair("IIII", "IV")
     );
 
 
@@ -51,9 +57,9 @@ public class RomanNumeral {
         return this;
     }
 
-    private void substituteSubStrings(List<Pair<String, String>> pairList) {
+    private void substituteSubStrings(List<StringPair> pairList) {
         for (Pair<String, String> p : pairList) {
-            this.value = this.value.replaceAll(p.getL(), p.getR());
+            this.value = this.value.replaceAll(p.getLeft(), p.getRight());
         }
     }
 
